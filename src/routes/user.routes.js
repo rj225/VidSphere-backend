23989e21b5b2b2c612468e9cd383ef0a92175fa4
Rishabh Raj <rajrishabh225@gmail.com>
2024-05-23 03:03:33 +0530

    removed verfication from channel dashboard

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -41,7 +41,7 @@ router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
-router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/c/:username").get(getUserChannelProfile)
 router.route("/history/:userId").get(verifyJWT, getWatchHistory);
 
 
